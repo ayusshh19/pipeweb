7f1855a8f26e2192f0bbfba85272d9f4c3d1764f
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-07-24 12:14:49 +0530

    added product page

--- pipeweb/develop/urls.py
+++ pipeweb/develop/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import home
+from .views import home,product
 
 urlpatterns = [
     path('',home,name='home'),
+    path('product/',product,name='product'),
 ]
