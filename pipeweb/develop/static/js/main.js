e3effc654ce6565945f9d70bae417d7c63ac982e
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-07-29 19:33:06 +0530

    made responsive index.html

--- pipeweb/develop/static/js/main.js
+++ pipeweb/develop/static/js/main.js
@@ -5,25 +5,25 @@ jQuery(document).ready(function() {
   slidesToShow: 8,
   autoplay: true,
   autoplaySpeed: 2000,
+  infinite: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         infinite: true,
-        dots: true
       }
     },
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 3,
+        slidesToScroll: 3
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 360,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
