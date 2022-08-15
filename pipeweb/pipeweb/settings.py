935220af92bce2f09696db3203e20093473c4e1e
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-08-15 16:09:54 +0530

    sponcers

--- pipeweb/pipeweb/settings.py
+++ pipeweb/pipeweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'q4)!w)(^sqjbso&7i8nb#k1k+y5mk^czta8ft3%wred)7vrqj0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
