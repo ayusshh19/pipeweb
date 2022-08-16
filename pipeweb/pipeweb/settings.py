c05771669a44a7b55956b96e6d191444a82d9577
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-08-16 16:08:03 +0530

    qoutation

--- pipeweb/pipeweb/settings.py
+++ pipeweb/pipeweb/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 import dj_database_url
-db_from_env=dj_database_url.config(conn_max_age=600, require_ssl=True)
+db_from_env=dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
