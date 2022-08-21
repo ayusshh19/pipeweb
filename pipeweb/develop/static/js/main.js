fcbffc360a71083c7125805a2679c0369549b293
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-08-21 22:30:46 +0530

    wp and indiamart

--- pipeweb/develop/static/js/main.js
+++ pipeweb/develop/static/js/main.js
@@ -31,6 +31,36 @@ jQuery(function() {
     }
   ]
 });
+jQuery('.aboutslider').slick({
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 })
 jQuery(document).ready(function() {
 jQuery('.mainslide').slick();
